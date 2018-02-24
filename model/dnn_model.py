fix accuracy computation

model/dnn_model.py
@@ -44,11 +44,12 @@ reg = tf.contrib.layers.l2_regularizer(0.01)
 hidden1 = tf.layers.dense(x, units=96, kernel_regularizer=reg, activation=tf.nn.elu, name='hidden1')
 dropout1 = tf.layers.dropout(hidden1, rate=0.2, training=training, name='dropout1')
 
-logits = tf.layers.dense(dropout1, units=classes, kernel_regularizer=reg, activation=tf.nn.relu, name='logits')
+logits = tf.layers.dense(dropout1, units=classes, kernel_regularizer=reg, activation=None, name='logits')
 loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y))
-prediction = tf.argmax(logits, axis=1)
-correct_predicted = tf.nn.in_top_k(logits, y, 1, name='in-top-k')
-wrong_predicted = tf.logical_not(correct_predicted, name='not-in-top-k')
+prediction = tf.argmax(logits, axis=1, name='prediction')
+predicted_distribution = tf.nn.softmax(logits, name='distribution')
+correct_predicted = tf.equal(tf.argmax(logits, 1), tf.cast(y, tf.int64), name='correct-prediction')
+wrong_predicted = tf.logical_not(correct_predicted, name='incorrect-prediction')
 x_misclassified = tf.boolean_mask(x, wrong_predicted, name='misclassified')
 accuracy = tf.reduce_mean(tf.cast(correct_predicted, tf.float32), name='accuracy')
 
@@ -63,6 +64,7 @@ tf.summary.scalar('accuracy', accuracy)
 tf.summary.histogram('x', x)
 tf.summary.histogram('hidden-activations-1', hidden1)
 tf.summary.histogram('logits', logits)
+tf.summary.histogram('predicted_distribution', predicted_distribution)
 for grad, var in grads_and_vars:
   name = var.name[:-2]
   tf.summary.histogram(name + '-var', var)
@@ -104,7 +106,7 @@ def train_step(batch_x, batch_y):
   if step % 10 == 0:
     cur_saver.save(sess, os.path.join(cur_ckpt_path, 'current'), step)
 
-current_top_accuracy = 0.95
+current_top_accuracy = 0.5
 
 def predict(files):
   all_acc = []