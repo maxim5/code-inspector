a23f3bf808eaab0ec710ef860e69ce71da338acd
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2018-03-03 21:24:41 +0100

    more tests

--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -42,6 +42,22 @@ class TokenizerTest(unittest.TestCase):
     text = '#include <math.h>'
     self.assertEqual(['#', 'include', ' ', '<', 'math', '.', 'h', '>'], list(tokenize_by_lexems(text)))
 
+  def test_identifier_1(self):
+    text = 'foo_bar'
+    self.assertEqual(['foo_bar'], list(tokenize_by_lexems(text)))
+
+  def test_identifier_2(self):
+    text = 'fooBar'
+    self.assertEqual(['fooBar'], list(tokenize_by_lexems(text)))
+
+  def test_identifier_3(self):
+    text = 'FooBar'
+    self.assertEqual(['FooBar'], list(tokenize_by_lexems(text)))
+
+  def test_identifier_4(self):
+    text = 'fooBar_FOOBar_foobar012'
+    self.assertEqual(['fooBar_FOOBar_foobar012'], list(tokenize_by_lexems(text)))
+
   def test_long(self):
     text = 'foo bar baz foo2  {x?_}!943 322 !# ^\t^& while (a<b): 0x00\n  int a[0:]'
     self.assertEqual(['foo', ' ', 'bar', ' ', 'baz', ' ', 'foo2', ' ', ' ', '{', 'x', '?', '_', '}!',
